14e4cc5805264ab02a0803b9e94aaa11d54b40b4
FenixAi <9mdgsvhgkb@privaterelay.appleid.com>
2026-01-06 09:15:49 +0300

    Delete backend directory

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-INPUT_DIR = os.path.join(BASE_DIR, "input")
-OUTPUT_DIR = os.path.join(BASE_DIR, "output")
-TMP_DIR = os.path.join(BASE_DIR, "tmp")
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
