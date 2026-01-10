ccf9018188eeaa587e764e997112131cfed8385c
FenixAi <9mdgsvhgkb@privaterelay.appleid.com>
2026-01-10 17:53:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -122,3 +122,16 @@ function uploadVideo(file, endpoint) {
     alert('Ошибка при загрузке видео');
   });
 }
+if (modal && modalContent && modalClose) {
+  modalClose.addEventListener('click', () => {
+    modal.classList.remove('active');
+    modalContent.classList.remove('show'); 
+  });
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modal.classList.remove('active');
+      modalContent.classList.remove('show'); 
+    }
+  });
+}
